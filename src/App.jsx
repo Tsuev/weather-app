52bb2430db993055bb870f852fb831431a527158
Tsuev Mansur <tsuevjudoka@gmail.com>
2023-02-26 22:11:07 +0300

    commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './assets/App.scss'
+
+function App() {
+  return (
+    <div className='app'>
+      <div className="app__left">
+
+      </div>
+      <div className="app__right">
+        
+      </div>
+    </div>
+  )
+}
+
+export default App
